a9ab1cd160c4c5a10e61e01ad4d1e5a1e4d9ecf5
Sander <sander@sander-K52JB.(none)>
2013-04-15 15:07:32 +0200

    began making phrase pair extractions

--- src/ppe.py
+++ src/ppe.py
@@ -2,10 +2,25 @@
 
 import argparse
 
-def extract_phrase_pairs(alignments, language1, language2):
+def extract_phrase_pair_freqs(alignments, language1, language2):
     phrase_pairs = []
-    # TODO
+    alignments = open(alignments, 'r')
+    language1 = open(language1, 'r')
+    language2 = open(language2, 'r')
     return phrase_pairs
+
+def extract_phrase_pairs(alignments):
+    # First form words into phrase pairs
+    while alignments:
+        rect_init = alignments.pop()
+        rect_exp = [[rect_init[0]], [rect_init[1]]]
+        while not (rect_exp[0] or rect_exp[1]):
+            added_points = [(x,y) for (x,y) in alignments if (x in rect_exp[0] or y in rect_exp[1])]
+            rect.append(added_points)
+            for point in added_points:
+                alignments.delete()
+            rect_exp = rect_expansions(rect)
+    #Then loop over phrase pairs to join them together into new ones
 
 if __name__=='__main__':
     arg_parser = argparse.ArgumentParser()
