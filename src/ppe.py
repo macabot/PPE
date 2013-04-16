ee362a3b32d0ccd1cf5274a1c0a9754b3ce5f343
Sander <sander@sander-K52JB.(none)>
2013-04-16 14:26:09 +0200

    ppe needs some debugging.

--- src/ppe.py
+++ src/ppe.py
@@ -53,12 +53,12 @@ def extract_alignments(word_alignments):
     phrase_alignment_list = []
     # First form words into phrase pairs
     while len(word_alignments):
-        phrase_alignment = []
+        phrase_alignment = set()
         phrase_alignment_init = word_alignments.pop()
         phrase_alignment_exp = [[phrase_alignment_init[0]], [phrase_alignment_init[1]]]
         while phrase_alignment_exp[0] or phrase_alignment_exp[1]:
-            added_points = [(x,y) for (x,y) in word_alignments if (x in phrase_alignment_exp[0] or y in phrase_alignment_exp[1])]
-            phrase_alignment.extend(added_points)
+            added_points = set([(x,y) for (x,y) in word_alignments if (x in phrase_alignment_exp[0] or y in phrase_alignment_exp[1])])
+            phrase_alignment |= added_points
             word_alignments -= added_points
             phrase_alignment_exp = phrase_alignment_expansions(phrase_alignment)
         phrase_alignment_list.append(phrase_range(phrase_alignment))
@@ -69,10 +69,18 @@ def extract_alignments(word_alignments):
         new_p3 = []
         for p2 in phrase_queue:
             p3 = None
-            #p2 above, to the left of p1
-            #if p1[0:2] == p2[2:4]:
-            #    p3 = p2[0], p2[1], p1[2], p1[3]
-            #elif p1[2] == p2[0] and p1[3]
+            if p1[0] is p2[2]+1 and p1[1] is p2[3]+1:
+                #p2 above, to the left of p1
+                p3 = p2[0], p2[1], p1[2], p1[3]
+            elif p1[2] is p2[0]-1 and p1[1] is p2[3]-1:
+                #p2 above, to the right of p1
+                p3 = p1[0], p2[1], p2[2], p1[3]
+            elif p1[0] is p2[2]+1 and p1[3] is p2[1]-1:
+                #p2 below, to the left of p1
+                p3 = p2[0], p1[1], p1[2], p2[3]
+            elif p1[2] is p2[0]-1 and p1[3] is p2[1]-1:
+                #p2 below, to the left of p1
+                p3 = p1[0], p1[1], p2[2], p2[3]
             if p3:
                 new_p3.append(p3)
         phrase_alignment_list.append(new_p3)
@@ -81,26 +89,26 @@ def extract_alignments(word_alignments):
     return phrase_alignment_list
 
 if __name__=='__main__':
-    l1 = "is not"
-    l2 = "n' est pas"
-    phrase_alignments = [(0,0,1,2), (0,1,0,1)]
-    generator = extract_phrase_pairs_gen(phrase_alignments, l1, l2)
-    print generator
-    for phrase_pair in generator:
-        print phrase_pair
-    '''
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-a", "--alignments",
         help="File containing alignments")
+    '''
     arg_parser.add_argument("-l1", "--language1",
         help="File containing sentences of language 1")
     arg_parser.add_argument("-l2", "--language2",
         help="File containing sentences of language 2")
+    '''
     
     args = arg_parser.parse_args()
     alignments = args.alignments
+    '''
     language1 = args.language1
     language2 = args.language2
     print alignments, language1, language2
-    phrase_pairs = extract_phrase_pairs(alignments, language1, language2)
     '''
+    af = open(alignments, 'r')
+    for a in af:
+        al = str_to_alignments(a)
+        print al
+        phrase_pairs = extract_alignments(al)
+        print phrase_pairs
