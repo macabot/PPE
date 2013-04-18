2c989e27633810aacb9bd8099dc9fc751f00d0a7
Sander <sander@sander-K52JB.(none)>
2013-04-18 16:05:31 +0200

    made function to check for concatenations in phrases

--- src/ppc.py
+++ src/ppc.py
@@ -8,3 +8,17 @@ def compare(train_files, held_out_files):
     held_out_freqs = ppe.extract_phrase_pair_freqs(held_out_files[0],
                     held_out_files[1], held_out_files[2], 
                     held_out_files[3])
+
+def compare_phrase(test_phrase, phrase_table, max_concat, concat_num):
+    if concat_num > max_concat:
+        return False
+    phrase_splits = split_phrase(test_phrase, concat_num)
+    for ps in phrase_splits:
+        match = True
+        for split in ps:
+            if split not in phrase_table:
+                match = False
+                break
+        if match:
+            return True
+    return compare_phrase(test_phrase, phrase_table, max_concat, concat_num+1)
